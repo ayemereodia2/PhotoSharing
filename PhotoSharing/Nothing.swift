b39f68f1577eb2d9d3cfafc19e392b21d591b28f
Ayemere Odia <Odiadavid2@gmail.com>
2020-07-05 15:03:04 +0100

    Added a sample file

---
+++ PhotoSharing/Nothing.swift
@@ -0,0 +1,13 @@
+//
+//  Nothing.swift
+//  PhotoSharing
+//
+//  Created by Ayemere  Odia  on 05/07/2020.
+//  Copyright © 2020 Ayemere  Odia . All rights reserved.
+//
+
+import Foundation
+
+class Nothing {
+    
+}
